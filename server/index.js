93423a4cb297dda395e8e207b73eb69a5244b081
Melvunx <augustine.melv@gmmail.com>
2024-11-11 15:52:40 +0100

    setup router

--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const pool = require("./config/database");
+
+const loginRoutes = require("./routes/login.routes");
+const todoRoutes = require("./routes/todo.routes");
+const notesRoutes = require("./routes/notes.routes");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", loginRoutes);
+app.use("/api/todo", todoRoutes);
+app.use("/api/notes", notesRoutes);
 
 const { SERVER_PORT } = process.env;
 
@@ -17,19 +23,19 @@ app.listen(SERVER_PORT, () => {
 
 app.get("/", (req, res) => res.send("Hello World"));
 
-app.get("/test", async (req, res) => {
-  const client = await pool.connect();
+// app.get("/test", async (req, res) => {
+//   const client = await pool.connect();
 
-  try {
-    const result = await client.query("SELECT * FROM account");
-    console.log(result.rows);
+//   try {
+//     const result = await client.query("SELECT * FROM account");
+//     console.log(result.rows);
 
-    res.json(result.rows);
-  } catch (error) {
-    console.error("Il y a une erreur ! ", error);
-  } finally {
-    client.release();
-  }
+//     res.json(result.rows);
+//   } catch (error) {
+//     console.error("Il y a une erreur ! ", error);
+//   } finally {
+//     client.release();
+//   }
 
-  res.status(404);
-});
+//   res.status(404);
+// });
