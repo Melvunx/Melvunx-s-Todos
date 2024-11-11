76d282d977a0322ea4ebc08945ea3893c465462a
Melvunx <augustine.melv@gmmail.com>
2024-11-11 16:19:52 +0100

    change login to auth

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
-const loginRoutes = require("./routes/login.routes");
+const authRoutes = require("./routes/auth.routes");
 const todoRoutes = require("./routes/todo.routes");
 const notesRoutes = require("./routes/notes.routes");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/api", loginRoutes);
+app.use("/api", authRoutes);
 app.use("/api/todo", todoRoutes);
 app.use("/api/notes", notesRoutes);
 
@@ -23,19 +23,3 @@ app.listen(SERVER_PORT, () => {
 
 app.get("/", (req, res) => res.send("Hello World"));
 
-// app.get("/test", async (req, res) => {
-//   const client = await pool.connect();
-
-//   try {
-//     const result = await client.query("SELECT * FROM account");
-//     console.log(result.rows);
-
-//     res.json(result.rows);
-//   } catch (error) {
-//     console.error("Il y a une erreur ! ", error);
-//   } finally {
-//     client.release();
-//   }
-
-//   res.status(404);
-// });
