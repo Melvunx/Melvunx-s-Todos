7e27e402aa00d693da4437b995861360ed066475
Melvunx <augustine.melv@gmmail.com>
2024-11-11 16:09:58 +0100

    presetup login

---
+++ server/controller/login.controller.js
@@ -0,0 +1,22 @@
+const pool = require("../config/database");
+
+module.exports.login = async (req, res) => {
+  try {
+  } catch (error) {
+  } finally {
+  }
+};
+
+module.exports.register = async (req, res) => {
+  try {
+  } catch (error) {
+  } finally {
+  }
+};
+
+module.exports.logout = async (req, res) => {
+  try {
+  } catch (error) {
+  } finally {
+  }
+};
--- server/routes/login.routes.js
+++ server/routes/login.routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { login, register, logout } = require("../controller/login.controller");
 
-
+router.use("/login", login);
+router.use("/register", register);
+router.use("/logout", logout);
 
 module.exports = router;
